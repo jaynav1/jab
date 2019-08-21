07f8e7c42bc74986923549bf1a257d81a2a6477a
Jaylen N <jaylen.nav@gmail.com>
2019-08-21 20:21:07 +1000

    add zenserp command

---
+++ commands/zenserpAsk.js
@@ -0,0 +1,10 @@
+const fetch = require("node-fetch")
+const Discord = require ('discord.js')
+
+module.exports = {
+	name: 'ask',
+	description: 'get results from google using zenserp',
+	execute(client, receivedMessage, arguments) {
+        
+	}
+}
