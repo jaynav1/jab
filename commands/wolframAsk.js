a6173a476468ffc3002a8954d87794c0c295c809
Jaylen N <jaylen.nav@gmail.com>
2019-08-21 20:54:25 +1000

    add waApi

--- commands/wolframAsk.js
+++ commands/wolframAsk.js
@@ -1,12 +1,13 @@
-const fetch = require("node-fetch")
 const Discord = require ('discord.js')
+const WolframAlphaAPI = require('wolfram-alpha-api')
+const waApi = WolframAlphaAPI('YWU7YQ-28GGQQH6V8')
 
 module.exports = {
 	name: 'ask',
 	description: 'get results from wolfram',
 	execute(client, receivedMessage, arguments) {
-        arguments = arguments.join('%20')
-        fetch(`http://api.wolframalpha.com/v1/result?appid=DEMO&i=How+far+is+Los+Angeles+from+New+York%3f`, { method: 'GET', headers: headers})
-            .then(res => console.log(res))
+		arguments = arguments.join('+')
+		waApi.
+        
 	}
 }
