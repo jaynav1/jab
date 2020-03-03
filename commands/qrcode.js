492073cb71a2f58161a60b076735d8926d977005
Jaylen N <jaylen.nav@gmail.com>
2020-03-03 21:34:54 +1100

    sure

--- commands/qrcode.js
+++ commands/qrcode.js
@@ -1,11 +1,11 @@
 const fetch = require("node-fetch")
-const Discord = require ('discord.js')
+const Discord = require('discord.js')
 
 module.exports = {
 	name: 'qrcode',
 	description: 'generate qr code',
 	execute(client, receivedMessage, arguments) {
-        fetch(`http://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${arguments}.png`)
+        fetch(`http://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=${arguments}`)
             .then(response => response.blob())
             .then(response => {
                 console.log(response)
